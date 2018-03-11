ad5d0a172aabfcbdf9c8f4c292586cd75a998269
chi <1410364@hcmut.edu.vn>
2018-03-11 21:35:24 +0700

    add timer for client

--- mesh/core/src/timer.c
+++ mesh/core/src/timer.c
@@ -325,7 +325,6 @@ void timer_on_ts_begin(timestamp_t timeslot_start_time)
     {
 #if !defined(HOST)
         (void) NVIC_EnableIRQ(TIMER0_IRQn);
-        (void) NVIC_EnableIRQ(TIMER1_IRQn);
 #endif
     }
 
